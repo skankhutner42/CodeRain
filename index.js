0281a697bf5d18a8c00a187080767835dad3089e
Lao Mountain Taoist <1979533752@qq.com>
2023-03-28 18:04:06 +0800

    CodeRain1.0

---
+++ index.js
@@ -0,0 +1,17 @@
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+canvas.width = screen.availWidth;
+canvas.height = screen.availHeight;
+var strArr = '010101010101101101110000110'.split('');
+var Arr = Array(Math.ceil(canvas.width / 10)).fill(0);
+console.log(Arr);
+var rain = function () {
+    ctx.fillStyle = 'rgba(0,0,0,0.05)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#0f0';
+    Arr.forEach(function (item, index) {
+        ctx.fillText(strArr[Math.floor(Math.random() * strArr.length)], index * 10, item + 10);
+        Arr[index] = item > canvas.height ? 0 : item + 10;
+    });
+};
+setInterval(rain, 40);
---
+++ index.ts
@@ -0,0 +1,20 @@
+let canvas = document.querySelector('canvas')
+let ctx = canvas.getContext('2d')
+canvas.width = screen.availWidth
+canvas.height = screen.availHeight
+
+let strArr = '010101010101101101110000110'.split('')
+let Arr = Array(Math.ceil(canvas.width / 10)).fill(0)
+console.log(Arr)
+
+const rain = () => {
+    ctx.fillStyle = 'rgba(0,0,0,0.05)'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = '#0f0'
+    Arr.forEach((item, index) => {
+        ctx.fillText(strArr[Math.floor(Math.random() * strArr.length)], index * 10, item + 10)
+        Arr[index] = item > canvas.height ? 0 : item + 10
+    })
+}
+
+setInterval(rain, 40)
